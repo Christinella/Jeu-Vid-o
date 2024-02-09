fc339af7ae49d7003ab237c50d43d7bcac981450
herivola <rchristinella@gmail.com>
2024-02-09 16:52:26 +0100

    Jeupasfini

--- script.js
+++ script.js
@@ -11,19 +11,45 @@ document.addEventListener("DOMContentLoaded", function () {
   function moveRight() {
     const currentLeft = parseInt(window.getComputedStyle(element).left);
     element.style.left = currentLeft + 40 + "px";
-
-
   }
+  
 
   document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowLeft") {
       moveLeft();
+      
     } else if (event.key === "ArrowRight") {
       moveRight();
     }
   });
 });
-document.getElementById("start").addEventListener("click" ,function(){
-  document.getElementById("start").style.display="none";
-  
-})
+const player = document.getElementById('element');
+const obstacle1 = document.getElementById('obstacle1');
+const score1 = document.getElementById('score');
+
+let score =0;
+
+function collision() {
+  const playerRect = player.getBoundingClientRect();
+  const obstacleReact1 = obstacle1.getBoundingClientRect();
+
+  if (
+playerRect.top < obstacleReact1.bottom &&
+playerRect.bottom > obstacleReact1.top &&
+playerRect.left < obstacleReact1.right &&
+playerRect.right > obstacleReact1.left 
+    ){
+clearInterval(scoreInterval);
+alert ('Tas trop! Your score is :' + score);
+    }
+  }
+const scoreInterval = setInterval(() => {
+  score++;
+  score1.textContent = score;
+},1000);
+
+setInterval(collision, 50);
+
+
+
+
